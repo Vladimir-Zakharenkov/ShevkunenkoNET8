b1b4ffdce02739d5ec405b34b1de74afdadb6ee6
Владимир Захаренков <zakharenkov1965@yandex.ru>
2024-08-02 19:47:53 +0300

    02-08-2024-19-47

--- ShevkunenkoSite.Services/SiteDbContext.cs
+++ ShevkunenkoSite.Services/SiteDbContext.cs
@@ -32,72 +32,72 @@ public class SiteDbContext : DbContext
             .Property(p => p.PagePathNickNameWithData)
             .HasComputedColumnSql("[PagePathNickName] + [RoutData]");
 
-        modelBuilder.Entity<MovieFileModel>()
-            .Property(p => p.PageInfoModelIdForSeries)
-            .HasDefaultValue(null);
+        //modelBuilder.Entity<MovieFileModel>()
+        //    .Property(p => p.PageInfoModelIdForSeries)
+        //    .HasDefaultValue(null);
 
-        modelBuilder.Entity<ImageFileModel>()
-            .Property(p => p.WebImageFileNameExtension)
-            .HasDefaultValue("webp");
+        //modelBuilder.Entity<ImageFileModel>()
+        //    .Property(p => p.WebImageFileNameExtension)
+        //    .HasDefaultValue("webp");
 
-        modelBuilder.Entity<ImageFileModel>()
-            .Property(p => p.WebImageMimeType)
-            .HasDefaultValue("image/webp");
+        //modelBuilder.Entity<ImageFileModel>()
+        //    .Property(p => p.WebImageMimeType)
+        //    .HasDefaultValue("image/webp");
 
-        modelBuilder.Entity<ImageFileModel>()
-            .Property(p => p.WebIconFileNameExtension)
-            .HasDefaultValue("webp");
+        //modelBuilder.Entity<ImageFileModel>()
+        //    .Property(p => p.WebIconFileNameExtension)
+        //    .HasDefaultValue("webp");
 
-        modelBuilder.Entity<ImageFileModel>()
-            .Property(p => p.WebIconMimeType)
-            .HasDefaultValue("image/webp");
+        //modelBuilder.Entity<ImageFileModel>()
+        //    .Property(p => p.WebIconMimeType)
+        //    .HasDefaultValue("image/webp");
 
-        modelBuilder.Entity<ImageFileModel>()
-            .Property(p => p.WebIcon200FileNameExtension)
-            .HasDefaultValue("webp");
+        //modelBuilder.Entity<ImageFileModel>()
+        //    .Property(p => p.WebIcon200FileNameExtension)
+        //    .HasDefaultValue("webp");
 
-        modelBuilder.Entity<ImageFileModel>()
-            .Property(p => p.WebIcon200MimeType)
-            .HasDefaultValue("image/webp");
+        //modelBuilder.Entity<ImageFileModel>()
+        //    .Property(p => p.WebIcon200MimeType)
+        //    .HasDefaultValue("image/webp");
 
-        modelBuilder.Entity<ImageFileModel>()
-            .Property(p => p.WebIcon100FileNameExtension)
-            .HasDefaultValue("webp");
+        //modelBuilder.Entity<ImageFileModel>()
+        //    .Property(p => p.WebIcon100FileNameExtension)
+        //    .HasDefaultValue("webp");
 
-        modelBuilder.Entity<ImageFileModel>()
-            .Property(p => p.WebIcon100MimeType)
-            .HasDefaultValue("image/webp");
+        //modelBuilder.Entity<ImageFileModel>()
+        //    .Property(p => p.WebIcon100MimeType)
+        //    .HasDefaultValue("image/webp");
 
-        modelBuilder.Entity<ImageFileModel>()
-            .Property(p => p.WebImageHDNameExtension)
-            .HasDefaultValue("webp");
+        //modelBuilder.Entity<ImageFileModel>()
+        //    .Property(p => p.WebImageHDNameExtension)
+        //    .HasDefaultValue("webp");
 
-        modelBuilder.Entity<ImageFileModel>()
-            .Property(p => p.WebImageHDMimeType)
-            .HasDefaultValue("image/webp");
+        //modelBuilder.Entity<ImageFileModel>()
+        //    .Property(p => p.WebImageHDMimeType)
+        //    .HasDefaultValue("image/webp");
 
-        modelBuilder.Entity<PageInfoModel>()
-            .Property(p => p.PageLinks)
-            .HasDefaultValue(false);
+        //modelBuilder.Entity<PageInfoModel>()
+        //    .Property(p => p.PageLinks)
+        //    .HasDefaultValue(false);
 
-        modelBuilder.Entity<PageInfoModel>()
-            .Property(p => p.RefPages)
-            .HasDefaultValue(string.Empty);
+        //modelBuilder.Entity<PageInfoModel>()
+        //    .Property(p => p.RefPages)
+        //    .HasDefaultValue(string.Empty);
 
-        modelBuilder.Entity<MovieFileModel>()
-           .Property(p => p.FramesAroundMovie)
-           .HasDefaultValue(string.Empty);
+        //modelBuilder.Entity<MovieFileModel>()
+        //   .Property(p => p.FramesAroundMovie)
+        //   .HasDefaultValue(string.Empty);
 
-        modelBuilder.Entity<MovieFileModel>()
-           .Property(p => p.SeriesSearchFilter)
-           .HasDefaultValue(string.Empty);
+        //modelBuilder.Entity<MovieFileModel>()
+        //   .Property(p => p.SeriesSearchFilter)
+        //   .HasDefaultValue(string.Empty);
 
-        modelBuilder.Entity<PageInfoModel>()
-           .Property(p => p.PageFilter)
-           .HasDefaultValue(string.Empty);
+        //modelBuilder.Entity<PageInfoModel>()
+        //   .Property(p => p.PageFilter)
+        //   .HasDefaultValue(string.Empty);
 
-        modelBuilder.Entity<PageInfoModel>()
-          .Property(p => p.PageFilterOut)
-          .HasDefaultValue(string.Empty);
+        //modelBuilder.Entity<PageInfoModel>()
+        //  .Property(p => p.PageFilterOut)
+        //  .HasDefaultValue(string.Empty);
     }
 }
