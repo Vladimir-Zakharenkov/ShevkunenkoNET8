3ca783b848427a8aeae10d4d83202b00921a1cb2
Владимир Захаренков <zakharenkov1965@yandex.ru>
2024-08-15 19:09:18 +0300

    15-08-2024-19-08

--- Metanit.com/Руководство по ASP.NET Core 8/Глава 06 - Кофигурация/Chapter06-03/Program.cs
+++ Metanit.com/Руководство по ASP.NET Core 8/Глава 06 - Кофигурация/Chapter06-03/Program.cs
@@ -1,11 +1,30 @@
 #region Example01
 
+//var builder = WebApplication.CreateBuilder();
+//var app = builder.Build();
+
+//builder.Configuration.AddJsonFile("config.json");
+
+//app.Map("/", (IConfiguration appConfig) => $"{appConfig["person"]} - {appConfig["company"]}");
+
+//app.Run();
+
+#endregion
+
+#region Example02
+
 var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
-builder.Configuration.AddJsonFile("config.json");
+builder.Configuration.AddJsonFile("config2.json");
 
-app.Map("/", (IConfiguration appConfig) => $"{appConfig["person"]} - {appConfig["company"]}");
+app.Map("/", (IConfiguration appConfig) =>
+{
+    var personName = appConfig["person:profile:name"];
+    var companyName = appConfig["company:name"];
+
+    return $"{personName} - {companyName}";
+});
 
 app.Run();
 
