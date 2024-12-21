05d703e026353ec0b42ae0b4e62438ae7f6fe222
Владимир Захаренков <zakharenkov1965@yandex.ru>
2024-12-21 13:21:50 +0300

    21-12-2024-13-21

--- Adam Freeman ProAspNetCore7/SportsStore/Controllers/HomeController.cs
+++ Adam Freeman ProAspNetCore7/SportsStore/Controllers/HomeController.cs
@@ -67,6 +67,43 @@
 
 #region Listing 7.31 Updating the action method in the HomeController.cs file in the Sports Store / Controllers folder
 
+//using Microsoft.AspNetCore.Mvc;
+//using SportsStore.Models;
+//using SportsStore.Models.ViewModels;
+
+//namespace SportsStore.Controllers
+//{
+//    public class HomeController : Controller
+//    {
+//        private IStoreRepository repository;
+
+//        public int PageSize = 4;
+//        public HomeController(IStoreRepository repo)
+//        {
+//            repository = repo;
+//        }
+
+//        public ViewResult Index(int productPage = 1) => View(new ProductsListViewModel
+//        {
+//            Products = repository.Products
+//                .OrderBy(p => p.ProductID)
+//                .Skip((productPage - 1) * PageSize)
+//                .Take(PageSize),
+
+//            PagingInfo = new PagingInfo
+//            {
+//                CurrentPage = productPage,
+//                ItemsPerPage = PageSize,
+//                TotalItems = repository.Products.Count()
+//            }
+//        });
+//    }
+//}
+
+#endregion
+
+#region Listing 8.2. Supporting categories in the HomeController.cs file in the SportsStore/Controllers folder
+
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
 using SportsStore.Models.ViewModels;
@@ -78,25 +115,32 @@ namespace SportsStore.Controllers
         private IStoreRepository repository;
 
         public int PageSize = 4;
+
         public HomeController(IStoreRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult Index(int productPage = 1) => View(new ProductsListViewModel
-        {
-            Products = repository.Products
-                .OrderBy(p => p.ProductID)
-                .Skip((productPage - 1) * PageSize)
-                .Take(PageSize),
+        public ViewResult Index(string? category, int productPage = 1) =>
+            View
+            (
+                new ProductsListViewModel
+                {
+                    Products = repository.Products.Where(p => category == null || p.Category == category)
+                        .OrderBy(p => p.ProductID)
+                        .Skip((productPage - 1) * PageSize)
+                        .Take(PageSize),
 
-            PagingInfo = new PagingInfo
-            {
-                CurrentPage = productPage,
-                ItemsPerPage = PageSize,
-                TotalItems = repository.Products.Count()
-            }
-        });
+                    PagingInfo = new PagingInfo
+                    {
+                        CurrentPage = productPage,
+                        ItemsPerPage = PageSize,
+                        TotalItems = repository.Products.Count()
+                    },
+
+                    CurrentCategory = category
+                }
+             );
     }
 }
 
--- Adam Freeman ProAspNetCore7/SportsStore/Models/ViewModels/ProductsListViewModel.cs
+++ Adam Freeman ProAspNetCore7/SportsStore/Models/ViewModels/ProductsListViewModel.cs
@@ -1,5 +1,19 @@
 ﻿#region Listing 7.30 The contents of the ProductsListViewModel.cs file in the SportsStore/Models / ViewModels folder
 
+//namespace SportsStore.Models.ViewModels
+//{
+//    public class ProductsListViewModel
+//    {
+//        public IEnumerable<Product> Products { get; set; } = Enumerable.Empty<Product>();
+
+//        public PagingInfo PagingInfo { get; set; } = new();
+//    }
+//}
+
+#endregion
+
+#region Listing 8.1 Modifying the ProductsListViewModel.cs file in the SportsStore/Models/ViewModels folder
+
 namespace SportsStore.Models.ViewModels
 {
     public class ProductsListViewModel
@@ -7,6 +21,8 @@ namespace SportsStore.Models.ViewModels
         public IEnumerable<Product> Products { get; set; } = Enumerable.Empty<Product>();
 
         public PagingInfo PagingInfo { get; set; } = new();
+
+        public string? CurrentCategory { get; set; }
     }
 }
 
