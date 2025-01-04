d091e794bb1e8d0d6f721eaa1b1c90f57f10c241
Владимир Захаренков <zakharenkov1965@yandex.ru>
2025-01-05 00:50:58 +0300

    05-01-2025-00-50

--- ShevkunenkoSite.Models/ViewModels/EditMovieViewModel.cs
+++ ShevkunenkoSite.Models/ViewModels/EditMovieViewModel.cs
@@ -3,54 +3,54 @@
 public class EditMovieViewModel : DetailsMovieViewModel
 {
     [DataType(DataType.Upload)]
-    [Display(Name = "Файл фильма:")]
+    [Display(Name = "Файл фильма :")]
     public IFormFile? FileForMovieFormFile { get; set; }
 
     [DataType(DataType.Upload)]
-    [Display(Name = "Полный вариант фильма:")]
+    [Display(Name = "Полный вариант фильма :")]
     public IFormFile? FullMovieFormFile { get; set; }
 
     [DataType(DataType.Upload)]
-    [Display(Name = "Картинка фильма:")]
+    [Display(Name = "Картинка фильма :")]
     public IFormFile? ImageForMovieFormFile { get; set; }
 
     [DataType(DataType.Upload)]
-    [Display(Name = "Постер фильма:")]
+    [Display(Name = "Постер фильма :")]
     public IFormFile? PosterForMovieFormFile { get; set; }
 
     [DataType(DataType.Upload)]
-    [Display(Name = "Картинка страницы серий:")]
+    [Display(Name = "Картинка страницы серий :")]
     public IFormFile? ImageHeadForSeriesFormFile { get; set; } = null;
 
     [Required(AllowEmptyStrings = true)]
     [DisplayFormat(ConvertEmptyStringToNull = false)]
     [DataType(DataType.Text)]
-    [Display(Name = "Страница фильма:")]
+    [Display(Name = "Страница фильма :")]
     public string PageForMovie { get; set; } = string.Empty;
 
     [Required(AllowEmptyStrings = true)]
     [DisplayFormat(ConvertEmptyStringToNull = false)]
     [DataType(DataType.Text)]
-    [Display(Name = "Страница серий:")]
+    [Display(Name = "Страница серий :")]
     public string PageForSeriesString { get; set; } = string.Empty;
 
     [DisplayFormat(ConvertEmptyStringToNull = false)]
     [DataType(DataType.Text)]
-    [Display(Name = "Темы видео:")]
+    [Display(Name = "Темы видео :")]
     public List<TopicMovieModel> TopicsForMovie { get; set; } = [];
 
     [DisplayFormat(ConvertEmptyStringToNull = false)]
     [DataType(DataType.Text)]
-    [Display(Name = "Тип картинки для ссылок (1):")]
+    [Display(Name = "Тип картинки для ссылок (1) :")]
     public string ImageTypeForRef1 { get; set; } = string.Empty;
 
     [DisplayFormat(ConvertEmptyStringToNull = false)]
     [DataType(DataType.Text)]
-    [Display(Name = "Тип картинки для ссылок (2):")]
+    [Display(Name = "Тип картинки для ссылок (2) :")]
     public string ImageTypeForRef2 { get; set; } = string.Empty;
 
     [DisplayFormat(ConvertEmptyStringToNull = false)]
     [DataType(DataType.Text)]
-    [Display(Name = "Тип картинки для ссылок (3):")]
+    [Display(Name = "Тип картинки для ссылок (3) :")]
     public string ImageTypeForRef3 { get; set; } = string.Empty;
 }
