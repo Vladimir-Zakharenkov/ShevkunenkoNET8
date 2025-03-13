1639b564e9ad2eab730d772b9c87f5f8300f8817
Владимир Захаренков <zakharenkov1965@yandex.ru>
2025-03-13 22:00:25 +0300

    13-03-2025-22-00

--- ShevkunenkoSite.Models/ViewModels/ArticleViewModel.cs
+++ ShevkunenkoSite.Models/ViewModels/ArticleViewModel.cs
@@ -2,5 +2,9 @@
 
 public class ArticleViewModel
 {
+    // данные статьи или книги
     public BooksAndArticlesModel BookOrArticle { get; set; } = new();
+
+    // данные страницы (статьи или книги)
+    public PageInfoModel PageInfo { get; set; } = new();
 }
--- ShevkunenkoSite/Controllers/ArticlesController.cs
+++ ShevkunenkoSite/Controllers/ArticlesController.cs
@@ -1,21 +1,27 @@
 ﻿namespace ShevkunenkoSite.Controllers;
 
-public class ArticlesController (
+public class ArticlesController(
     IBooksAndArticlesRepository articleContext,
-    ITextInfoRepository textContext) : Controller
+    ITextInfoRepository textContext,
+    IPageInfoRepository pageContext) : Controller
 {
     public async Task<IActionResult> Index(Guid? articleId, int? pageNumber)
     {
-        if (articleId != null 
+        if (articleId != null
             && pageNumber != null
-            && await articleContext.BooksAndArticles.Where(article => article.BooksAndArticlesModelId == articleId).AnyAsync() 
+            && await articleContext.BooksAndArticles.Where(article => article.BooksAndArticlesModelId == articleId).AnyAsync()
             && await textContext.Texts.Where(text => text.BooksAndArticlesModelId == articleId).AnyAsync()
             && pageNumber > 0
             && pageNumber <= articleContext.BooksAndArticles.First(article => article.BooksAndArticlesModelId == articleId).NumberOfPages)
         {
             ArticleViewModel bookOrArticle = new()
             {
-                BookOrArticle = await articleContext.BooksAndArticles.FirstAsync(article => article.BooksAndArticlesModelId == articleId)
+                BookOrArticle = await articleContext.BooksAndArticles
+                    .Include(logo => logo.LogoOfArticle)
+                    .Include(scan => scan.ScanOfArticle)
+                    .FirstAsync(article => article.BooksAndArticlesModelId == articleId),
+
+                PageInfo = await pageContext.GetPageInfoByPathAsync(HttpContext)
             };
 
             return View("Article", bookOrArticle);
