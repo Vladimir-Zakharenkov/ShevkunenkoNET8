d678ce6fc62f9cb6d9f485f690649f2158b452da
Владимир Захаренков <zakharenkov1965@yandex.ru>
2025-10-22 07:25:16 +0300

    22-10-2025-07-25

--- ShevkunenkoSite.Services/Migrations/20251021200954_21-10-2025-23-07.Designer.cs
+++ ShevkunenkoSite.Services/Migrations/20251021200954_21-10-2025-23-07.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShevkunenkoSite.Services;
 
@@ -11,9 +12,11 @@ using ShevkunenkoSite.Services;
 namespace ShevkunenkoSite.Services.Migrations
 {
     [DbContext(typeof(SiteDbContext))]
-    partial class SiteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251021200954_21-10-2025-23-07")]
+    partial class _211020252307
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
