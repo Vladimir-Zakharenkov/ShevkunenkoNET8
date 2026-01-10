76d80024a36dd48f2409d43dfa53fcf2d05295d6
Владимир Захаренков <zakharenkov1965@yandex.ru>
2026-01-11 01:08:54 +0300

    11-01-2026-01-08

---
+++ PowerShell/search-in-subfolder.ps1
@@ -0,0 +1,13 @@
+﻿<#
+В этом примере выполняется поиск определенной текстовой строки в каталоге и всех его подкаталогах.
+#>
+Get-ChildItem -Path C:\Windows\System32\*.txt -Recurse |
+    Select-String -Pattern 'Microsoft' -CaseSensitive
+<#
+Get-ChildItem использует параметр Path для указания C:\Windows\System32*.txt. 
+Параметр Recurse включает в себя поддиректории. 
+Объекты отправляются по конвейеру в Select-String.
+Select-String использует параметр Pattern и указывает строку Microsoft. 
+Параметр CaseSensitive используется для точного сопоставления регистра строки. 
+Select-String отображает выходные данные в консоли PowerShell.
+#>
